fd667eca557cc2c2a2bab4cc33a26113bb0abe84
arnaud-vanderschriek <arnaud.vanderschrieck@gmail.com>
2022-12-07 11:08:21 +0100

    modify indent

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,36 +4,42 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+let name = " arnaud";
 
 const Projects = () => (
-  <Section nopadding id='projects'>
-    <SectionDivider />
-    <SectionTitle main>Projects</SectionTitle>
-    <GridContainer>
-      {projects.map(({ id, image, title, description, tags,source, visit}) => (
-       <BlogCard key={id}>
-        <Img src={image} />
-          <TitleContent>
-            <HeaderThree title={title}></HeaderThree>
-            <Hr />
-          </TitleContent>
-          <CardInfo>{description}</CardInfo>
-          <div>
-            <TitleContent>Stack</TitleContent>
-            <TagList>
-              {tags.map((tag, index) => (
-                <Tag key={index}>{tag}</Tag>
-              ))}
-            </TagList>
-          </div>
-          <UtilityList>
-            <ExternalLinks href={visit}>Code</ExternalLinks>
-            <ExternalLinks href={source}>Source</ExternalLinks>
-          </UtilityList>
-       </BlogCard>
-      ))}
-    </GridContainer>
-  </Section>
+  <div>
+    hebentoi
+    { name}
+  </div>
+ // <Section nopadding id='projects'>
+ //   <SectionDivider />
+ //   <SectionTitle main>Projects</SectionTitle>
+ //   <GridContainer>
+ //     {projects.map(({ id, image, title, description, tags,source, visit}) => (
+ //      <BlogCard key={id}>
+ //       <Img src={image} />
+ //         <TitleContent>
+ //           <HeaderThree title={title}></HeaderThree>
+ //           <Hr />
+ //         </TitleContent>
+ //         <CardInfo>{description}</CardInfo>
+ //         <div>
+ //           <TitleContent>Stack</TitleContent>
+ //           <TagList>
+ //             {tags.map((tag, index) => (
+ //               <Tag key={index}>{tag}</Tag>
+ //             ))}
+ //           </TagList>
+ //         </div>
+ //         <UtilityList>
+ //           <ExternalLinks href={visit}>Code</ExternalLinks>
+ //           <ExternalLinks href={source}>Source</ExternalLinks>
+ //         </UtilityList>
+ //      </BlogCard>
+ //     ))}
+ //   </GridContainer>
+ // </Section>
 );
+
 
 export default Projects;
